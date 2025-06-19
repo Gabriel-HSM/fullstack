01e8d0d42cee5f741199476dacbe3aa4d2cdf734
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2025-06-19 13:11:48 -0300

    Praticando Swich Case

---
+++ JavaScript/exercicio7.js
@@ -0,0 +1,15 @@
+let opcao = 1
+
+switch(opcao){
+    case 1:
+        console.log("Você escolheu Sorvete")
+        break;
+    case 2:
+        console.log("Você escoheu Pizza")
+        break;
+    case 3:
+        console.log("Você escoheu Hambúrger")
+        break;
+    default:
+        console.log("Opção inválida! Escolha de 1 ~ 3 ")
+}
