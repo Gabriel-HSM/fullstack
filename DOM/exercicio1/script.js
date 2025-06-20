598f93e38ce17defecd3380d747aa2ed1894fe9e
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2025-06-20 16:42:25 -0300

    Praticando DOM

---
+++ DOM/exercicio1/script.js
@@ -0,0 +1,3 @@
+function clicou(){
+ document.getElementById("txtclique").innerHTML = "Clicou!"
+}
---
+++ DOM/exercicio2/script.js
@@ -0,0 +1,6 @@
+function clicou(){
+
+    let nome = document.getElementById("nomeUsuario").value
+
+    document.getElementById("boasVindas").innerHTML = `Olá, ${nome}`
+}
