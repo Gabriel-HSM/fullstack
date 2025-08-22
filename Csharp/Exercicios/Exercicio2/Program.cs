29e63ef68d1e6964eadd73e5cbb0ef684be04713
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2025-08-22 17:05:28 -0300

    Finalização de exercicio e entendendo, métodos, propriedades e contrutores

---
+++ Csharp/Exercicios/Exercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿using Exercicio2.models;
+
+Pessoa p1 = new Pessoa(nome: "Gabriel", sobrenome: "Xavier");
+
+Pessoa p2 = new Pessoa("Eduardo","Neves");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+// p1.Nome = "Gabriel";
+// p1.Sobrenome = "Xavier";
+// p1.Idade = 20;
+// p1.Apresentar();
---
+++ Csharp/Exercicios/Exercicio2/models/Pessoa.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercicio2.models
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+            
+        }
+        public Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+
+        }
+        private string _nome;
+        private int _Idade;
+        
+        public string Nome
+        {
+            get => _nome.ToUpper();
+            
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+                _nome = value;
+            }
+        }
+        public string Sobrenome { get; set; }
+        public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+        public int Idade
+        {
+            get => _Idade;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentNullException("A idade não pode ser menor que zero");
+                }
+                _Idade = value;
+            }
+        }
+
+
+
+        public void Apresentar()
+        {
+            System.Console.WriteLine($"Nome: {NomeCompleto}, Idade: {Idade}");
+        }
+    }
+}
