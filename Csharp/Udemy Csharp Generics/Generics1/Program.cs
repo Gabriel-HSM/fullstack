fb234d6a0109c4a3e5eb7b2c593df4c6413b9cf4
Gabriel Henrique <gabriel.henrique.sirgh442@gmail.com>
2026-02-03 15:52:44 -0300

    Aprendendo sobre tipo genérico

---
+++ Csharp/Udemy Csharp Generics/Generics1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Generics1.services;
+
+PrintService<int> printService = new PrintService<int>();
+
+Console.WriteLine("How many values? ");
+
+string? imput = Console.ReadLine();
+if (string.IsNullOrEmpty(imput))
+{
+    throw new ArgumentException("A entrada não pode ser nula");
+}
+
+int n = int.Parse(imput);
+
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    printService.AddValue(x);
+}
+
+printService.Print();
+Console.WriteLine("First " + printService.First());
---
+++ Csharp/Udemy Csharp Generics/Generics1/services/PrintService.cs
@@ -0,0 +1,45 @@
+namespace Generics1.services
+{
+    public class PrintService<T>
+    {
+        //vetor simples, pode armazenar 10 inteiros
+        private T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValue(T v)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("Print Service is full");
+            }
+
+            _values[_count] = v;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("Print Service is empty");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+           Console.Write("[");
+           for (int i = 0; i < _count -1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+
+            if(_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+            }
+            Console.WriteLine("]");
+
+        }
+    }
+}
